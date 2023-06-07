fb6eaffd04729e6dce1c705f656ee24bb2d1b4d5
esquared13 <erin_enzinger@yahoo.com>
2023-06-06 23:26:46 -0400

    first commit with some setup

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Net;
+using System.IO;
+using System.Drawing;
+
+namespace Roadkill
+{
+    public class photos
+    {
+        string [] urls = {};
+        // create a loop that changes the imageURL variable to the different URLs and downloads all of the images, also add a changing title variable
+        // create a variable that pulls the user name for the file location
+
+        public void download(string imageURL)
+        {
+            try
+            {
+                
+                
+            }
+            catch
+            {
+
+            }
+            
+        }
+    }
+}
+
+
+
+
+
+// code snippet for download and destination
+//public class Example
+//{
+ //   public static void Main()
+   // {
+     //   String url = "https://code.jquery.com/jquery-3.6.1.min.js";
+       // string dest = @"D:\jquery-3.6.1.min.js";
+ //
+   //     using (StreamReader reader = new StreamReader(WebRequest.Create(url)
+     //           .GetResponse().GetResponseStream()))
+       // {
+         //   String content = reader.ReadToEnd();
+           // File.WriteAllText(dest, content);
+      //  }    
+    //}
+//}
