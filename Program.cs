9169367c4653c94cd7efde9f6bdbbc04d7ca4d6d
esquared13 <erin_enzinger@yahoo.com>
2023-06-07 19:42:13 -0400

    added links and more to-do list

--- Program.cs
+++ Program.cs
@@ -6,14 +6,35 @@ using System.Net;
 using System.IO;
 using System.Drawing;
 
-namespace Roadkill
+namespace roadkill
 {
-    public class photos
+    class game
     {
-        string [] urls = {};
+        // add getters and setters and constructor and all that silly stuff
+        // need to create a method for the game i think maybe
+        private static int honkfrequency = 10000; // might need to change this
+        private static int honkduration = 100; // might need to change this
+        
+
+    }
+    class photos
+    {
+        string [] urls = // array of roadkill photo urls
+        {
+            "https://i1.wp.com/www.denverpost.com/wp-content/uploads/2016/08/20160804_101741_raccoon.jpg?w=810&crop=0%2C0px%2C100%2C9999px",
+            "https://radraccoon.files.wordpress.com/2016/08/ta9cmoy-e1470405823516.jpg?w=1108",
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fgraspingforobjectivity.com%2F2017%2F01%2Fon-creating-a-roadkill-kit.html%2F&psig=AOvVaw1-r8I3vhGZ4Dv7fRPPlxE4&ust=1686192239184000&source=images&cd=vfe&ved=2ahUKEwjIzeTckbD_AhWepIkEHe0_DBYQjRx6BAgAEAw",
+            "https://images5.memedroid.com/images/UPLOADED11/50bbdca9dc5b7.jpeg",
+            "https://4.bp.blogspot.com/_EfDRu0IfQ6s/Se8XsTmtJcI/AAAAAAAAFOc/-gt0O4w2npE/s400/Krystal+King.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjvU83MByLRiij1_tHs0OokmCjMOAte3scWA&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjYZTNhkC95pUvZRqfkoSB5f8YwurW_ZlHYA&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJIG_zyqeY8phRfW2KNSR7xUlGFLrBVmqrew&usqp=CAU"
+        };
+        
+        // add getters and setters and constructor and all that jazz
         // create a loop that changes the imageURL variable to the different URLs and downloads all of the images, also add a changing title variable
         // create a variable that pulls the user name for the file location
-
+    
         public void download(string imageURL)
         {
             try
