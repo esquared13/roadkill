1b8d775c02d49f435441590afe91891f6e6eec63
esquared13 <erin_enzinger@yahoo.com>
2023-06-08 11:34:30 -0400

    testing and fixing and whatnot

--- Program.cs
+++ Program.cs
@@ -8,16 +8,11 @@ using System.Drawing;
 using System.Numerics;
 using System.Timers;
 
-// hello before you start on this again, you might need to rearrange some things since there are two endings to the game
-// the photos probably shouldn't be its own class, and it might all need to be in the game (or same class hehe) class
-
-
-namespace roadkill
+namespace Roadkill
 {
-    class game
+    class Game
     {
-        // add getters and setters and constructor and all that silly stuff
-        // need to create a method for the game i think maybe
+        // add getters and setters and constructor and all that silly stuff or also don't that's fine
         public static string traffic = @"
         
                                                          _________________________   
@@ -28,63 +23,74 @@ namespace roadkill
 `````````````````````````````````````````````````````````````````````````````````````
 
         ";
+        private static string username = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\')[1]; // split takes domain name out of username
         private static int honkfrequency; 
         private static int honkduration; 
         private static bool ending;
-        private static string username = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
         private static TimeSpan waittime;
+        private static string [] urls = // array of roadkill photo urls
+        {
+            "https://i1.wp.com/www.denverpost.com/wp-content/uploads/2016/08/20160804_101741_raccoon.jpg?w=810&crop=0%2C0px%2C100%2C9999px",
+            "https://radraccoon.files.wordpress.com/2016/08/ta9cmoy-e1470405823516.jpg?w=1108",
+            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fgraspingforobjectivity.com%2F2017%2F01%2Fon-creating-a-roadkill-kit.html%2F&psig=AOvVaw1-r8I3vhGZ4Dv7fRPPlxE4&ust=1686192239184000&source=images&cd=vfe&ved=2ahUKEwjIzeTckbD_AhWepIkEHe0_DBYQjRx6BAgAEAw",
+            "https://images5.memedroid.com/images/UPLOADED11/50bbdca9dc5b7.jpeg",
+            "https://4.bp.blogspot.com/_EfDRu0IfQ6s/Se8XsTmtJcI/AAAAAAAAFOc/-gt0O4w2npE/s400/Krystal+King.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjvU83MByLRiij1_tHs0OokmCjMOAte3scWA&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjYZTNhkC95pUvZRqfkoSB5f8YwurW_ZlHYA&usqp=CAU",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJIG_zyqeY8phRfW2KNSR7xUlGFLrBVmqrew&usqp=CAU"
+        };
 
-        bool gameplay()
+        static void Main()
         {
             Console.Title = "cyberpunk2077.exe";
-            Console.Write("Please enter your full legal name, billing address, credit card number, cvv, and expiration date.");
+            Console.WriteLine("Please enter your full legal name, billing address, credit card number, cvv, and expiration date.");
             Console.ReadKey();
-            Console.Write("Just kidding lol.");
+            Console.WriteLine("\nJust kidding lol.");
 
             while(true) // user input determines the frequency of the console beep
             {        
-                Console.Write("Pick a number.");
-                    
-                if(int.TryParse(Console.ReadLine(), out int result))
+                
+                Console.WriteLine("Pick a number between 37 and 32767."); // this is the only range of integers that works for the beep frequency
+  
+                if(int.TryParse(Console.ReadLine(), out int frequency))
                 {
-                    honkfrequency = Convert.ToInt32(Console.ReadLine());
-                    break;
+                    if(frequency >= 37 && frequency <= 32767)
+                    {
+                        honkfrequency = frequency;
+                        break;
+                    }
                 }
-                else
-                {
-                        Console.Write("Try again loser!");
-                        
-                }
+                Console.WriteLine("Try again loser!");         
             }
             while(true) // user input determines the duration of the console beep.  tread lightly young padawan.
             {
-                Console.Write("Pick another number."); 
+                Console.WriteLine("Pick a number between 1000 and 100000."); 
 
-                if(int.TryParse(Console.ReadLine(), out int result))
+                if(int.TryParse(Console.ReadLine(), out int duration))
                 {
-                    honkduration = Convert.ToInt32(Console.ReadLine());
-                    break;
+                    if(duration >= 1000 && duration <= 100000)
+                    {
+                        honkduration = duration;
+                        break;
+                    }
                 }
-                else
-                {
-                        Console.Write("Stop trying to be different and just pick a number.");                       
-                }
+                        Console.WriteLine("Stop trying to be different and just pick a number.");                       
             }    
 
             // probably need to clean up this dialog and make it look prettier and add spaces and whatnot        
-            Console.Write(traffic);
+            Console.WriteLine(traffic);
             Console.Beep(honkfrequency, honkduration);
-            Console.Write("That sound? That's traffic.");
-            Console.Write("Wait, what was your name again?");
+            Console.WriteLine("That was a honk.");
+            Console.WriteLine("Wait, what was your name again?");
             Console.ReadKey();
-            Console.Write("Just kidding.  How could I forget!");
-            Console.Write("You, " + username + " are sitting in the car, trying to leave town after work.  It is approximately 5:15pm EST.  Traffic is moving slowly.");
+            Console.WriteLine("\nJust kidding.  How could I forget!");
+            Console.WriteLine("You, " + username + ", are sitting in the car, trying to leave town after work.  It is approximately 5:15pm EST.  Traffic is moving slowly.");
             
 
             // this next part really is going to need some cooking on 
             while(true) // user input determines the length of the timer countdown thing
             {
-                Console.Write("Pick a number."); 
+                Console.WriteLine("Pick a number."); 
                 try
                 {
                     waittime = TimeSpan.Parse(Console.ReadLine());
@@ -107,26 +113,15 @@ namespace roadkill
                 //    ArgumentOutOfRangeException
             
 
-            // return statement here please gal
-            return ending; // will be equal to true or false and determine what consequence
+            // no return but have an outcome that determines the other things
         }
         
         
 
     }
-    class photos
+    class Photos
     {
-        string [] urls = // array of roadkill photo urls
-        {
-            "https://i1.wp.com/www.denverpost.com/wp-content/uploads/2016/08/20160804_101741_raccoon.jpg?w=810&crop=0%2C0px%2C100%2C9999px",
-            "https://radraccoon.files.wordpress.com/2016/08/ta9cmoy-e1470405823516.jpg?w=1108",
-            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fgraspingforobjectivity.com%2F2017%2F01%2Fon-creating-a-roadkill-kit.html%2F&psig=AOvVaw1-r8I3vhGZ4Dv7fRPPlxE4&ust=1686192239184000&source=images&cd=vfe&ved=2ahUKEwjIzeTckbD_AhWepIkEHe0_DBYQjRx6BAgAEAw",
-            "https://images5.memedroid.com/images/UPLOADED11/50bbdca9dc5b7.jpeg",
-            "https://4.bp.blogspot.com/_EfDRu0IfQ6s/Se8XsTmtJcI/AAAAAAAAFOc/-gt0O4w2npE/s400/Krystal+King.jpg",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjvU83MByLRiij1_tHs0OokmCjMOAte3scWA&usqp=CAU",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjYZTNhkC95pUvZRqfkoSB5f8YwurW_ZlHYA&usqp=CAU",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJIG_zyqeY8phRfW2KNSR7xUlGFLrBVmqrew&usqp=CAU"
-        };
+
         
         // add getters and setters and constructor and all that jazz
         // create a loop that changes the imageURL variable to the different URLs and downloads all of the images, also add a changing title variable
