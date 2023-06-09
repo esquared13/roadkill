75c97daf0b1d8f131f9b34e3f0d126465ef8b359
Enzinger <eenzinge@iu.edu>
2023-06-09 14:38:51 -0400

    started on timer

--- Program.cs
+++ Program.cs
@@ -24,10 +24,11 @@ namespace Roadkill
 
         ";
         private static string username = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\')[1]; // split takes domain name out of username
+        private static string timerinput;
         private static int honkfrequency; 
-        private static int honkduration; 
+        private static int honkduration;
+        private static int seconds;
         private static bool ending;
-        private static TimeSpan waittime;
         private static string [] urls = // array of roadkill photo urls
         {
             "https://i1.wp.com/www.denverpost.com/wp-content/uploads/2016/08/20160804_101741_raccoon.jpg?w=810&crop=0%2C0px%2C100%2C9999px",
@@ -85,28 +86,21 @@ namespace Roadkill
             Console.ReadKey();
             Console.WriteLine("\nJust kidding.  How could I forget!");
             Console.WriteLine("You, " + username + ", are sitting in the car, trying to leave town after work.  It is approximately 5:15pm EST.  Traffic is moving slowly.");
-            
+    
 
             // this next part really is going to need some cooking on 
             while(true) // user input determines the length of the timer countdown thing
             {
-                Console.WriteLine("Pick a number."); 
-                try
-                {
-                    waittime = TimeSpan.Parse(Console.ReadLine());
-                }
-                catch
+                Console.WriteLine("Pick a number.");
+                timerinput = Console.Readline();
+
+                if(!int.TryParse(timerinputer, out seconds) || seconds <= 0)
                 {
 
                 }
+                    
 
-
-              
-            }  
-
-    
-            // test    
-
+            } 
                 // some exceptions to handle hehehehehe   
                 //    IOException
                 //    OutOfMemoryException
@@ -115,9 +109,6 @@ namespace Roadkill
 
             // no return but have an outcome that determines the other things
         }
-        
-        
-
     }
     class Photos
     {
@@ -162,3 +153,63 @@ namespace Roadkill
       //  }    
     //}
 //}
+
+
+
+// another snippet
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int seconds;
+
+        while (true)
+        {
+            Console.Write("Enter the number of seconds for the timer: ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out seconds) || seconds <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer.");
+                continue;
+            }
+
+            Timer timer = new Timer(seconds * 1000);
+            timer.Elapsed += TimerElapsed;
+
+            timer.Start();
+
+            Console.WriteLine("Press Enter to start another timer or 'q' to quit.");
+            string choice = Console.ReadLine();
+
+            if (choice == "q")
+                break;
+
+            timer.Stop();
+        }
+    }
+
+    static void TimerElapsed(object sender, ElapsedEventArgs e)
+    {
+        Console.WriteLine("Timer finished!");
+    }
+}
+This code allows the user to enter the number of seconds for the timer, starts the timer, and displays the "Timer finished!" message when the timer elapses. After each timer completes, the user can choose to start another timer or quit the program by pressing 'q'.
+
+
+
+
+
+User
+If the input cannot be parsed, will the loop start back at "Enter the number of seconds
+ChatGPT
+Yes, if the input cannot be parsed or if the entered number of seconds is invalid (less than or equal to 0), the loop will start back at the "Enter the number of seconds" prompt. The continue statement is used to skip the rest of the loop iteration and start the next iteration immediately.
+
+In the provided example, if the user enters an invalid input, such as a non-numeric value or a negative number, the code will display the error message and then continue to the next iteration of the loop, prompting the user again to enter a valid number of seconds. This ensures that the user is prompted until a valid input is provided.
+
+
+
+
+
+
